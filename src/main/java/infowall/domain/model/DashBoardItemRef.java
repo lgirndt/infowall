4b4261c9520ebca85784f394a216be2fa32d02c4
Lars Girndt <l.girndt@googlemail.com>
2010-12-09 23:26:59 +0100

    added missing file

--- src/main/java/infowall/domain/model/DashBoardItemRef.java
+++ src/main/java/infowall/domain/model/DashBoardItemRef.java
@@ -3,5 +3,29 @@ package infowall.domain.model;
 /**
  *
  */
-public class DashBoardItemRef {
+public class DashboardItemRef {
+
+    private String dashboardId;
+    private String itemName;
+
+    public DashboardItemRef(String dashboardId, String itemName) {
+        this.dashboardId = dashboardId;
+        this.itemName = itemName;
+    }
+
+    public String getDashboardId() {
+        return dashboardId;
+    }
+
+    public void setDashboardId(String dashboardId) {
+        this.dashboardId = dashboardId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
 }
