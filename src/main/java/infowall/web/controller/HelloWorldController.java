67ee495ab6bf735ade79479533bdafa4524719e7
Lars Girndt <l.girndt@googlemail.com>
2010-12-09 14:11:09 +0100

    added slf4j support with log4j backend and configuration

--- src/main/java/infowall/web/controller/HelloWorldController.java
+++ src/main/java/infowall/web/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package infowall.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloWorldController {
 
+    private final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
     @RequestMapping(value="/hello/{helloId}",method = RequestMethod.GET)
     public ModelAndView helloWorld(@PathVariable long helloId) {
+
+        logger.info("some log messsage.");
 
         String message = "Hello World, me " + helloId;
         return new ModelAndView("hello", "message", message);
