36d45781ec8e1c7dbdf930c27e1b5fb1b16ad7d7
Lars Girndt <l.girndt@googlemail.com>
2010-12-09 09:58:26 +0100

    spring starts

---
+++ src/main/java/infowall/web/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package infowall.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public ModelAndView helloWorld() {
+
+        String message = "Hello World, Spring 3.0!";
+        return new ModelAndView("hello", "message", message);
+    }
+}
