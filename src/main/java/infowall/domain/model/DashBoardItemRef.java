cdca25bc79e3e45794b0cd057ee7091363effec4
Lars Girndt <l.girndt@googlemail.com>
2010-12-11 12:32:52 +0100

    ?

--- src/main/java/infowall/domain/model/DashBoardItemRef.java
+++ src/main/java/infowall/domain/model/DashBoardItemRef.java
@@ -28,4 +28,24 @@ public class DashboardItemRef {
     public void setItemName(String itemName) {
         this.itemName = itemName;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DashboardItemRef that = (DashboardItemRef) o;
+
+        if (dashboardId != null ? !dashboardId.equals(that.dashboardId) : that.dashboardId != null) return false;
+        if (itemName != null ? !itemName.equals(that.itemName) : that.itemName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = dashboardId != null ? dashboardId.hashCode() : 0;
+        result = 31 * result + (itemName != null ? itemName.hashCode() : 0);
+        return result;
+    }
 }
