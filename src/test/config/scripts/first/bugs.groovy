61a3503ab11d788a49e2e4b6a59b7f35a2ddf900
Lars Girndt <l.girndt@googlemail.com>
2010-12-14 09:35:09 +0100

    fixed example

--- src/test/config/scripts/first/bugs.groovy
+++ src/test/config/scripts/first/bugs.groovy
@@ -2,12 +2,10 @@ import infowall.infrastructure.json.SimpleJSONBuilder
 
 def builder = new SimpleJSONBuilder()
 
-builder.array(['feck', 'arse', 'girls']) {
-      foo(bar:'1', baz: '2') {
+builder.foo(bar:'1', baz: '2') {
           hobbies(sport: 'snooker', drink: 'guinness')
           emptyObj([:])
           emptyArray([])
-      }
 };
 
-System.out << builder.json
+print builder.json
