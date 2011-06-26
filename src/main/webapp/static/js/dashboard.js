4c42021c8af26257e24c7c0b369e4a9c09ff50a5
Lars Girndt <l.girndt@googlemail.com>
2011-06-26 11:51:37 +0200

    encapsulated the animation.

--- src/main/webapp/static/js/dashboard.js
+++ src/main/webapp/static/js/dashboard.js
@@ -150,7 +150,7 @@
         else {
             return 'fail';
         }
-    }
+    };
 
     SingleValueView.prototype.relation = function(name,a,b){
         var rel;
@@ -291,6 +291,35 @@
 
     };
 
+    var slideShowAnimation = {
+        slide : {
+            initNewSlide : function(elem) {
+                var e = $(elem);
+                var height = e.height();
+                e.offset({left:0,top:height});
+            },
+            newSlideAnimation : function(elem) {
+                var width = $(elem).width();
+                return {top:-width};
+            },
+            currentSlideAnimation : function(elem) {
+                return {top:0};
+            }
+        },
+        fade : {
+            initNewSlide : function(elem) {
+                var e = $(elem);
+                e.offset({opacity:1});
+            },
+            newSlideAnimation : function(elem) {
+                return {opacity:0};
+            },
+            currentSlideAnimation : function(elem) {
+                return {opacity:1};
+            }
+        }
+    };
+
     SlideShow = function(opts){
         this.renderEngine = opts.renderEngine;
         this.items = opts.items;
@@ -298,8 +327,7 @@
         this.delay = (opts.delay || 5) * 1000;
         this.fxDuration = opts.fxDuration || 600;
         this.currItemId = 0;
-
-
+        this.slideShowAnimation = slideShowAnimation[ opts.animation || 'slide' ];
     };
 
     SlideShow.prototype.start = function(){
@@ -332,22 +360,36 @@
     };
 
     SlideShow.prototype.replaceView = function(item){
+        var self = this;
         var elem = $('<div class="view"></div>').appendTo(this.container);
         var width = $(this.container).width();
         var height = $(this.container).height();
-        console.log("height:" + height);
         var fxDuration = this.fxDuration;
         $(elem)
             .width(width)
-            .height(height)
-            .offset({left:0,top:height});
+            .height(height);
+        this.initNewSlide(elem);
         this.renderEngine.renderItem(item.name,elem,function(){
             var views = $('.view',this.container);
-            $(elem).animate( {top:0},{duration: fxDuration} );
+            var currentAnimation = self.currentSlideAnimation(elem);
+            var newAnimation = self.newSlideAnimation(elem);
+            $(elem).animate( currentAnimation,{duration: fxDuration} );
             if(views.length > 1){
-                $(views[0]).animate({top:-width},fxDuration,function(){$(this).remove()});
+                $(views[0]).animate(newAnimation,fxDuration,function(){$(this).remove()});
             }
         });
+    };
+
+    SlideShow.prototype.initNewSlide = function(elem){
+        return this.slideShowAnimation.initNewSlide(elem);
+    };
+
+    SlideShow.prototype.newSlideAnimation = function(elem){
+        return this.slideShowAnimation.newSlideAnimation(elem);
+    };
+
+    SlideShow.prototype.currentSlideAnimation = function(elem){
+        return this.slideShowAnimation.currentSlideAnimation(elem);
     };
 
     SlideShow.prototype.nextItem = function(){
