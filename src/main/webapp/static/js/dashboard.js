7cce722da1aea0b92a48863be15a03238563c674
Lars Girndt <lars.girndt@brands4friends.de>
2013-02-17 12:39:47 +0100

    Use actual data from model in pie chart.

--- src/main/webapp/static/js/dashboard.js
+++ src/main/webapp/static/js/dashboard.js
@@ -189,48 +189,34 @@
     };
 
     ChartView.prototype.transformModel = function(model,item) {
-        return model.current;
+        return model.current.data;
     };
 
-    ChartView.prototype.onRender = function(html) {
+    ChartView.prototype.onRender = function(html,model) {
         $elem = $(html);
         var canvasWidth = $elem.width(), //width
         canvasHeight = $elem.height(), //height
         outerRadius = ($elem.height() * 0.7 ) / 2, //radius
         color = d3.scale.category20(); //builtin range of colors
 
-        var dataSet = [
-            {"legendLabel":"Firefox", "magnitude":2},
-            {"legendLabel":"Two", "magnitude":3},
-            {"legendLabel":"Three", "magnitude":2},
-            {"legendLabel":"Four", "magnitude":16},
-            {"legendLabel":"Five", "magnitude":50},
-            {"legendLabel":"Six", "magnitude":8},
-            {"legendLabel":"Seven", "magnitude":30}
-        ];
-
-        // besser machen mit selecten
         var vis = d3.select($elem.find('.chart')[0])
         .append("svg:svg")//create the SVG element inside the <body>
-        .data([dataSet])//associate our data with the document
+        .data([model.data])//associate our data with the document
         .attr("width", canvasWidth)//set the width of the canvas
         .attr("height", canvasHeight)//set the height of the canvas
         .append("svg:g")//make a group to hold our pie chart
-        .attr("transform", "translate(" + 1.5 * outerRadius + "," + 1.5 * outerRadius + ")") // relocate center of pie to 'outerRadius,outerRadius'
+        .attr("transform", "translate(" + 1.5 * outerRadius + "," + 1.5 * outerRadius + ")"); // relocate center of pie to 'outerRadius,outerRadius'
 
         // This will create <path> elements for us using arc data...
         var arc = d3.svg.arc()
         .innerRadius(outerRadius * 0.6)
         .outerRadius(outerRadius);
-//
 
         var pie = d3.layout.pie()//this will create arc data for us given a list of values
         .value(function (d) {
-            return d.magnitude;
+            return d.value;
         })// Binding each value to the pie
-        .sort(function (d) {
-            return null;
-        });
+//        .sort(null);
 
         // Select all <g> elements with class slice (there aren't any yet)
         var arcs = vis.selectAll("g.slice")
@@ -277,8 +263,8 @@
         .attr("dy", legendTextSize + "px")
         .style("font","normal " + legendTextSize + "px 'Droid Serif'")
         .text(function(d) {
-            var val = dataSet[d];
-            return val.legendLabel + " " + val.magnitude + "%";
+            var val = model.data[d];
+            return val.label + " " + val.value+ "%";
         });
     };
 
@@ -400,7 +386,7 @@
                 $(dest).html(html);
                 console.log("hasonRender?");
                 if('onRender' in view) {
-                    view.onRender(dest[0]);
+                    view.onRender(dest[0], model);
                 }
                 if(callback){
                     callback();
